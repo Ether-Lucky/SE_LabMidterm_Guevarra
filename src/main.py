6d4bc5fcbc74694e3b61dede5b67bcdee668a424
Ether-Lucky <luckyguevarra.ph@gmail.com>
2026-04-11 00:37:57 +0800

    move: moved main.py under root

--- src/main.py
+++
@@ -1,7 +0,0 @@
-from src.view.view import CalculatorView
-from src.controller.controller import CalculatorController
-
-if __name__ == "__main__":
-    app = CalculatorView()
-    controller = CalculatorController(app)
-    app.mainloop()
